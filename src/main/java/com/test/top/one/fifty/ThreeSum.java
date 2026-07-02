ac985ee334ac0cef3e0b22d026a8bff17083a55b
vivek mishra <vivek.mishra661@gmail.com>
2026-07-02 12:36:54 +0530

    LeetCode initial commit

--- src/main/java/com/test/top/one/fifty/ThreeSum.java
+++ src/main/java/com/test/top/one/fifty/ThreeSum.java
@@ -1,31 +1,82 @@
 package com.test.top.one.fifty;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ThreeSum {
 
+    static void main() {
+        ThreeSum threeSum = new ThreeSum();
+        var t  = threeSum.threeSum(new int[]{-1,0,1,2,-1,-4});
+        t.toString();
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
         List<Integer> minHeap = new ArrayList<>();
-        List<Integer> maxHeap = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                minHeap.add(nums[i]);
-            } else if (nums[i] < 0) {
-                maxHeap.add(nums[i]);
-            }
+        List<List<Integer>> q = new ArrayList<>();
+        Set<Integer> pq = new HashSet<>();
+        for(int i = 0; i< nums.length;i++){
+            pq.add(nums[i]);
+            minHeap.add(nums[i]);
         }
-        minHeap.sort((a, b) -> b-a);
-        maxHeap.sort(Comparator.naturalOrder());
+        if(pq.size() ==1 && nums.length>2 && nums[0]==0){
+             q.add(List.of(0,0,0));
+             return q;
+        }
+        minHeap =  minHeap.stream().sorted().collect(Collectors.toList());
 
-       /* for(int i = ; i< minHeap.size(); i++){
+        Set<String> pp = new HashSet<>();
+        for (int i = 1; i < minHeap.size() - 1; i++) {
+            int pointer1 = i - 1;
+            int pointer2 = i + 1;
+            while (true) {
+                var value = minHeap.get(i) + minHeap.get(pointer1) + minHeap.get(pointer2);
+                if (value > 0) {
+                    if (pointer1 > 0) {
+                        pointer1--;
+                    } else {
+                        break;
+                    }
+                } else if (value < 0) {
+                    if (pointer2 < minHeap.size() - 1) {
+                        pointer2++;
 
-        }*/
+                    } else {
+                        break;
+                    }
+                } else {
+                    var y = new ArrayList<Integer>();
+                    y.add(minHeap.get(i));
+                    y.add(minHeap.get(pointer1));
+                    y.add(minHeap.get(pointer2));
+                    var yy = y.stream().map(e -> String.valueOf(e)).collect(Collectors.joining("-"));
+                    if (!pp.contains(yy)) {
+                        var t = new ArrayList<Integer>();
+                        t.add(minHeap.get(i));
+                        t.add(minHeap.get(pointer1));
+                        t.add(minHeap.get(pointer2));
+                        q.add(t);
+                        pp.add(yy);
 
-        return null;
+                    }
+                    if (pointer1 > 0) {
+                        pointer1--;
+                    } else {
+                        break;
+                    }
 
+                    if (pointer2 < minHeap.size() - 1) {
+                        pointer2++;
+
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+
+        }
+        return q;
 
     }
 }
